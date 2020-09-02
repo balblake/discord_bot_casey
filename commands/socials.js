0cb465eb771da550e767ed4cf859a014b3c730d5
B50 <B50@users.noreply.github.com>
2020-09-02 12:56:05 -0500

    reset

--- commands/socials.js
+++ commands/socials.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'socials',
     description: "All casey's socials",
     execute(message, args){
-        message.channel.send('Twitch: https://www.twitch.tv/CrazyCasey\n Youtube: https://www.youtube.com/channel/UC6KLE9UZ2pfQNOBkUONfHcw\n Twitter: https://twitter.com/CrazyCaseyy\n Instagram: https://www.instagram.com/crazycaseytv/\n Tiktok: https://www.tiktok.com/@crazycaseytv/ ');
+        message.channel.send('Twitch: https://www.twitch.tv/CrazyCasey\nYoutube: https://www.youtube.com/channel/UC6KLE9UZ2pfQNOBkUONfHcw\nTwitter: https://twitter.com/CrazyCaseyy\nInstagram: https://www.instagram.com/crazycaseytv/\nTiktok: https://www.tiktok.com/@crazycaseytv/');
     }
 } 
