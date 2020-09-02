5746a2810bd423907b0c4a57e787f1a87b97e3ca
B50 <B50@users.noreply.github.com>
2020-09-02 12:50:29 -0500

    Update socials.js

--- commands/socials.js
+++ commands/socials.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'socials',
     description: "All casey's socials",
     execute(message, args){
-        message.channel.send('**In Development**');
+        message.channel.send('Twitch: https://www.twitch.tv/CrazyCasey\n Youtube: https://www.youtube.com/channel/UC6KLE9UZ2pfQNOBkUONfHcw\n Twitter: https://twitter.com/CrazyCaseyy\n Instagram: https://www.instagram.com/crazycaseytv/\n Tiktok: https://www.tiktok.com/@crazycaseytv/ ');
     }
 } 
