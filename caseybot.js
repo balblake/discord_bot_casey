078331eec7175ebca89fdd9260cd0ca72a87326e
B50gamer <B50@users.noreply.github.com>
2020-09-06 10:46:36 -0700

    member

--- caseybot.js
+++ caseybot.js
@@ -74,8 +74,8 @@ client.on('message', message => {
                 return message.reply("You didnt specify a time!");
             }
  
-            Member.roles.add(mainrole.id)
-            Member.roles.remove(role.id);
+            member.roles.add(mainrole.id)
+            member.roles.remove(role.id);
  
  
             message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
