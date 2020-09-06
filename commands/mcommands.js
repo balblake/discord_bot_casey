bbeedf5ca37e66c50452be399fc96b26bc2974a2
B50gamer <B50@users.noreply.github.com>
2020-09-06 14:20:44 -0700

    1

--- commands/mcommands.js
+++ commands/mcommands.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'mcommands',
     description: "All the mod commands",
     execute(message, args){
-
+        message.delete();
+        
         if(message.member.roles.cache.has('386344247843880960')){
         message.channel.send('!kick (name), !ban (name), !clear (1-99), !mute (name), !unmute (name)');
 
