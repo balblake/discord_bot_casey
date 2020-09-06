eec0257fa8c2508bfc54c99daf182b894b6677d1
B50gamer <B50@users.noreply.github.com>
2020-09-06 12:58:16 -0700

    1

--- commands/mute.js
+++ commands/mute.js
@@ -7,8 +7,8 @@ module.exports = {
             var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
             if(!person) return  message.reply("I CANT FIND THE USER " + person)
  
-            var role = guildMember.roles.cache.get(role => role.name === 'Trucker babies');
-            var role2 = guildMember.roles.cache.get(role => role.name === '⛔Muted⛔');
+            var role = guild.member.roles.cache.get('386344485761843220');
+            var role2 = guild.member.roles.cache.get('752212210666831954');
             person.roles.add(role2);
             person.roles.remove(role);
            
