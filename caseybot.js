560be5a24cd0eceb69b90b7a375c54219e9f92b1
B50gamer <B50@users.noreply.github.com>
2020-09-06 10:10:05 -0700

    !mute command

--- caseybot.js
+++ caseybot.js
@@ -19,6 +19,48 @@ client.once('ready', () => {
     console.log('Casey Bot is online!');
 });
 
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(" ");
+ 
+    switch (args[0]) {
+        case 'mute':
+            var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+            if(!person) return  message.reply("I CANT FIND THE USER " + person)
+ 
+            let mainrole = message.guild.roles.find(role => role.name === "Trucker babies");
+            let role = message.guild.roles.find(role => role.name === "⛔Muted⛔");
+           
+ 
+            if(!role) return message.reply("Couldn't find the mute role.")
+ 
+ 
+            let time = args[2];
+            if(!time){
+                return message.reply("You didnt specify a time!");
+            }
+ 
+            person.removeRole(mainrole.id)
+            person.addRole(role.id);
+ 
+ 
+            message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
+ 
+            setTimeout(function(){
+                
+                person.addRole(mainrole.id)
+                person.removeRole(role.id);
+                console.log(role.id)
+                message.channel.send(`@${person.user.tag} has been unmuted.`)
+            }, ms(time));
+ 
+ 
+    
+        break;
+    }
+ 
+ 
+});
+
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
  
--- commands/mcommands.js
+++ commands/mcommands.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args){
 
         if(message.member.roles.cache.has('386344247843880960')){
-        message.channel.send('!kick (name), !ban (name), !clear (1-99)');
+        message.channel.send('!kick (name), !ban (name), !clear (1-99), !mute (name) (1s,1m,1h)');
 
 
 
