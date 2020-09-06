2533272784280702c30f29b7d417297945c9011d
B50gamer <B50@users.noreply.github.com>
2020-09-06 10:12:00 -0700

    client

--- caseybot.js
+++ caseybot.js
@@ -54,7 +54,7 @@ client.once('ready', () => {
     memberCount(client)
 });
 
-bot.on('message', message => {
+client.on('message', message => {
     let args = message.content.substring(PREFIX.length).split(" ");
  
     switch (args[0]) {
