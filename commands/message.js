3c6301b366c89a5d6f988d96e9579f3d8b2cabc5
B50gamer <B50@users.noreply.github.com>
2020-09-07 16:33:36 -0700

    1

---
+++ commands/message.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'message',
+    description: "send a message through the bot",
+    execute(message, args){
+        message.delete();
+
+if(message.member.roles.cache.has('386344247843880960')){
+    if (message.content.startsWith("!message")) {
+        // Get the channel mention
+        if (message.mentions.channels.size == 0) {
+            message.reply("please mention a channel first.");
+        }
+        else {
+            let targetChannel = message.mentions.channels.first();
+            // Get the message to print
+
+            const args = message.content.split(" ").slice(2);
+            let saytext = args.join(" ");
+            targetChannel.send(saytext);
+            message.delete();
+
+        }
+        }
+    }
+}};
