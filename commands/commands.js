d85b05db3a03b754fa67cb87159b8c00e31de7c1
B50gamer <B50@users.noreply.github.com>
2020-09-07 07:25:01 -0700

    1

--- commands/commands.js
+++ commands/commands.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'commands',
     description: "All the commands",
     execute(message, args){
-        message.channel.send('!twitch, !youtube, !yt, !twitch, !tiktok, !insta, !ig, !instagram, !socials');
+        message.channel.send('!twitch, !youtube, !yt, !tiktok, !insta, !ig, !instagram, !socials');
     }
 } 
