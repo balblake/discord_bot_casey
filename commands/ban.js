9832577b57c4108f951aedcb1a54f61cb801d1e9
B50 <B50@users.noreply.github.com>
2020-09-08 21:17:16 -0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,24 +4,29 @@ module.exports = {
     execute(message, args){
         if(message.member.roles.cache.has('386344247843880960')){
             const userBan = message.mentions.users.first();
-            const reason = message.args[2]();
+
             if (userBan) {
                 var member = message.guild.member(userBan)
-                
-                if(member) 
-                
-                        member.ban(reason)
-                    .then(() => {
-                        message.reply(`${userBan.tag} was banned from the server.`);
 
-                    }); else {
-                        message.reply('Make sure you state the reason or the user is not in the server.')
-                    }
+                if(member) {
+                    member.ban({
+                        reason: 'you broke the rules!'
+                    }).then(() => {
+                        message.reply(`${userBan.tag} was banned from the server.`)
+                    })
                 } else {
-                    message.reply('you need to sate the user you want to ban')
+                    message.reply('user is not in server');
                 }
             } else {
-                message.reply('Nice try! This is only for staff!');
+                message.reply('you need to sate the user you want to ban')
             }
+
+
+        } else {
+            message.reply('Nice try! This is only for staff!');
         }
+
+
+
     }
+}
