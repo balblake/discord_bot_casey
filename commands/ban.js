df96e0c9674088d5572b61d53f2ad610f406855f
B50 <B50@users.noreply.github.com>
2020-09-08 21:03:55 -0500

    pen

--- commands/ban.js
+++ commands/ban.js
@@ -2,16 +2,25 @@ module.exports = {
     name: 'ban',
     description: "bans user",
     execute(message, args){
-        message.delete();
+        if(message.member.roles.cache.has('386344247843880960')){
+            const userBan = message.mentions.users.first();
 
-if(message.member.roles.cache.has('746939181909016696')){;
-    const member = message.mentions.members.first(); // keep in mind it isn't the best practice to use message.mentions to retrieve an argument
-    if (!member) return message.channel.send('no member mentioned');
-    let reason = args.slice(2).join(' '); // arguments should already be defined
-    member.ban(reason)
-    .then(message.channel.send(`${userKick.tag} was banned from the server.`))
-    .catch(err => {
-      message.channel.send('something went wrong');
-      console.error();
-    });
-  }}}
+            if (userBan) {
+                var member = message.guild.member(userBan)
+                let reason = args.slice(2).join(' ');
+                if(member) 
+                        member.ban(reason)
+                    .then(() => {
+                        message.reply(`${userBan.tag} was banned from the server.`);
+
+                    }); else {
+                        message.reply('Make sure you state the reason or the user is not in the server.')
+                    }
+                } else {
+                    message.reply('you need to sate the user you want to ban')
+                }
+            } else {
+                message.reply('Nice try! This is only for staff!');
+            }
+        }
+    }
