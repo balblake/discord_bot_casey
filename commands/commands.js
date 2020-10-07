c947b3e8e60ca6066abb688b9778c6d074c62b1f
B50 <B50@users.noreply.github.com>
2020-10-07 18:43:13 -0500

    right cesu

--- commands/commands.js
+++ commands/commands.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'commands',
     description: "All the commands",
     execute(message, args){
+        message.delete();
         message.channel.send('!twitch, !youtube, !yt, !tiktok, !insta, !ig, !instagram, !socials');
     }
 } 
--- welcome.js
+++ welcome.js
@@ -1,11 +1,18 @@
+const { DiscordAPIError, Message } = require("discord.js");
+
 module.exports = client => {
     client.on('guildMemberAdd', member => {
     console.log('User ' + member.user.username + ' has joined the server!');
     var role = member.guild.roles.cache.find(role => role.name === 'Trucker babies');
-    member.roles.add(role);
+    member.roles.add(role)
     });
     client.on('guildMemberAdd', member => {
-    const targetChannelId = '707989016078843914'
-    member.guild.channels.cache.get('576617716023033865').send(`Welcome, **${member}** to casey's discord server!\nMake sure to check out his socials by going to ${member.guild.channels.cache.get(targetChannelId).toString()} and typing !socials`);
-    });
-}
+        const targetChannelId = '763475311114977300'
+        const targetChannelID2 = '751503236820893806'
+        const embed = new discord.RichEmbed()
+            .setTitle(`**${member}**`)
+            .setColor('BLUE')
+            .setDescription(`Welcome **${member}**!\nMake sure to read the **${member.guild.channels.cache.get(targetChannelId).toString()}**!\nCheck out Casey's **${member.guild.channels.cache.get(targetChannelID2).toString()}**!`)
+
+            message.channel.send(embed);
+    })}
