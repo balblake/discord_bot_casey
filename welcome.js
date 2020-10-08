59d394a44b629c02e2f5d946d592dfff655c320f
B50 <B50@users.noreply.github.com>
2020-10-07 19:22:54 -0500

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -4,7 +4,7 @@ const discord = require('discord.js')
 module.exports = client => {
     client.on('guildMemberAdd', member => {
     console.log('User ' + member.user.username + ' has joined the server!');
-    var role = member.guild.roles.cache.find('386344485761843220');
+    var role = member.guild.roles.cache.find(role => role.name === 'Trucker Babies');
     member.roles.add(role)
     });
     client.on('guildMemberAdd', member => {
