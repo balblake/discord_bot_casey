4fb7997b6b6689ae11c1acf12b2446c07883aaf3
B50 <B50@users.noreply.github.com>
2020-10-13 20:46:22 -0500

    Update embedmessage.js

--- commands/embedmessage.js
+++ commands/embedmessage.js
@@ -16,11 +16,12 @@ if(message.member.roles.cache.has('386344247843880960')){
             // Get the message to print
 
             const args = message.content.split(" ").slice(2);
+            const args1 = message.content.split(" ").slice(3);
             const embed = new discord.MessageEmbed()
             .setColor('BLUE')
-            .setTitle('CrazyCasey')
-            .setDescription(args)
-            .setTimestamp('10/13/2020')
+            .setTitle(args)
+            .setDescription(args1)
+            .setTimestamp('')
             targetChannel.send(embed);
             message.delete();
 
