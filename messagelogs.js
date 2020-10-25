47cbd3108443050eda7924bc9257140ee5bba437
B50gamer <B50@users.noreply.github.com>
2020-10-24 23:15:07 -0700

    message logs

---
+++ messagelogs.js
@@ -0,0 +1,20 @@
+const roles = ['Trucker Babies']
+
+module.exports = (client) => {
+    const channelId = '769796468055474206'
+
+    client.on('message', (message) => {
+        const { guild, content, member } = message
+
+        const hasRole = member.roles.cache.find((role) => {
+            return roles.includes(role.name)
+        })
+
+        if (hasRole) {
+            const channel = guild.channels.cache.get(channelId)
+            channel.send(`${message.author.username} said:
+            
+    "${content}"`)
+        }
+    })
+}
