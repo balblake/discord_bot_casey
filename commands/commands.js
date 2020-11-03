3b355156a5dbbe61af8b01f4b4b9cc80ff83a79b
B50 <B50@users.noreply.github.com>
2020-11-03 13:02:30 -0600

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,12 @@ module.exports = {
     name: 'commands',
     description: "All the commands",
     execute(message, args){
-        message.delete();
-        message.channel.send('!twitch, !youtube, !yt, !tiktok, !insta, !ig, !instagram, !socials');
-    }
-} 
+message.delete();
+const embed = new discord.MessageEmbed()
+.setColor('BLUE')
+.setTitle('Here are the commands.')
+.setTimestamp()
+.setFooter(':caseythumbsup:')
+.setDescription(`!socials - See all Casey's socials\n!twitch - Casey's Twitch Channel\n!youtube/!yt - Casey's YouTube Channel\n!tiktok - Casey's TikTok Page\n!instagram/!ig/!insta - Casey's Instagram Page`)
+message.channel.send(embed)
+}}
