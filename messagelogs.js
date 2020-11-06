06b503b77c19266c2a6ad2938fa94d61d6ff5144
B50 <B50@users.noreply.github.com>
2020-11-05 19:53:15 -0600

    Update messagelogs.js

--- messagelogs.js
+++ messagelogs.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
     client.on('message', (message) => {
         const { guild, content, member } = message
 
-        const hasRole = member.role.cache.find((role) => {
+        const hasRole = member.roles.cache.has((role) => {
             return roles.includes(role.name)
         })
 
