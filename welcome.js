07d7f3e42914c34057b4f33c3ad2fa2f8887f644
B50 <B50@users.noreply.github.com>
2020-11-05 21:04:10 -0600

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -4,11 +4,9 @@ const discord = require('discord.js')
 
 module.exports = client => {
     client.on('guildMemberAdd', member => {
-    console.log('User ' + member.user.username + ' has joined the server!');
-    var role = member.guild.roles.cache.find(role => role.name === 'Trucker Babies');
+    console.log('User ' + member.user.username + ' has joined the server. :)');
+    var role = member.guild.roles.cache.find(role => role.id === '386344485761843220');
     member.roles.add(role)
-    });
-    client.on('guildMemberAdd', member => {
         const targetChannelId = '763475311114977300'
         const targetChannelID2 = '751503236820893806'
         const targetChannelID3 = '707989016078843914'
@@ -16,7 +14,8 @@ module.exports = client => {
             .setTitle(`**Welcome to Casey's Discord!**`)
             .setTimestamp()
             .setColor('BLUE')
-            .setDescription(`Welcome **${member}!**\nMake sure to read the **${member.guild.channels.cache.get(targetChannelId).toString()}!**\nCheck out Casey's **${member.guild.channels.cache.get(targetChannelID2).toString()}!**\nDo !commands in **${member.guild.channels.cache.get(targetChannelID3).toString()}** to see the commands**!**`)
+            .setFooter('Have a good time!')
+            .setDescription(`Welcome **${member}**!\nMake sure to read the **${member.guild.channels.cache.get(targetChannelId).toString()}**!\nCheck out Casey's **${member.guild.channels.cache.get(targetChannelID2).toString()}**!\nDo !commands in **${member.guild.channels.cache.get(targetChannelID3).toString()}** to see the commands!`)
 
             member.guild.channels.cache.get('576617716023033865').send(embed);
     })}
